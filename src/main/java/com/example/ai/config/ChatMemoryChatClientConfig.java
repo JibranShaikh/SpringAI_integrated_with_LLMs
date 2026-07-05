f71eefa6085353bc46a8a1dcbb0751b69fae2e9f
Jibran <jibran.shaikh@hotmail.com>
2026-07-05 15:58:55 -0400

    Commits related to chatMemory

--- src/main/java/com/example/ai/config/ChatMemoryChatClientConfig.java
+++ src/main/java/com/example/ai/config/ChatMemoryChatClientConfig.java
@@ -4,7 +4,12 @@ package com.example.ai.config;
 import com.example.ai.advisors.TokenUsageAdvisor;
 import com.openai.models.ChatModel;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiChatOptions;
@@ -17,9 +22,28 @@ import java.util.List;
 @Configuration
 public class ChatMemoryChatClientConfig {
 
+
+
+    //MessageWindowChatMemory implements ChatMemory.
+    //In MessageWindowChatMemory a bean of ChatMemoryRepository is created.So,By default if
+    // JdbcChatMemoryRepository is present in the classpath, spring wont create bean of
+    // InMemoryChatRepository.
+    @Bean
+    public ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository){
+        return MessageWindowChatMemory.builder().maxMessages(10)
+                .chatMemoryRepository(jdbcChatMemoryRepository)
+                .build();
+    }
+
     @Bean("chatMemoryChatClient")
-    public ChatClient originalChatClient(OpenAiChatModel openAiChatModel){
+    public ChatClient originalChatClient(OpenAiChatModel openAiChatModel, ChatMemory chatMemory){
+        Advisor loggerAdvisor  = new SimpleLoggerAdvisor();
+        //You add chatMemory below because MessageChatMemoryAdvisor has its instance.
+        Advisor memoryAdvisor = MessageChatMemoryAdvisor
+                .builder(chatMemory)
+                .build();
         return ChatClient.builder(openAiChatModel)
+                .defaultAdvisors(List.of(loggerAdvisor, memoryAdvisor))
                 .build();
     }
 }
--- src/main/java/com/example/ai/controller/ChatMemoryController.java
+++ src/main/java/com/example/ai/controller/ChatMemoryController.java
@@ -2,17 +2,15 @@ package com.example.ai.controller;
 
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class ChatMemoryController {
-
 
     private final ChatClient chatClient;
 
@@ -20,11 +18,18 @@ public class ChatMemoryController {
         this.chatClient = chatClient;
     }
 
-    @GetMapping("/chat-memory")
-    public ResponseEntity<String> chatMemory(@RequestParam("message") String message){
+    @GetMapping("/chat-memory/{identifier}")
+    public ResponseEntity<String> chatMemory(
+            @RequestParam("message") String message,
+            @PathVariable("identifier") String identifier){
         //overriding the default system message.
         String content = chatClient
                 .prompt()
+                //here basically all the conversations that are going to happen will get the same
+                //convesation ids.
+                .advisors(advisorSpec -> {
+                    advisorSpec.param(ChatMemory.CONVERSATION_ID, identifier);
+                })
                 .user(message)
                 .call()
                 .content();
